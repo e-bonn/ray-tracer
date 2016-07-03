75457d23769091b0b2deb5b5e14a3f76302255e0
e-bonn <eribon41@gmail.com>
2016-07-03 01:16:20 -0400

    put RayTracing logic into a class

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -3,6 +3,8 @@
 
 #include "GeometryNode.hpp"
 #include <math.h>
+#include <thread>
+#include <vector>
 #include "RayTracer.hpp"
 
 using namespace std;
@@ -10,12 +12,29 @@ using namespace glm;
 
 static const int MAX_DEPTH = 4;
 
-void intersectsWithAny(Ray *ray, SceneNode *node, IntersectionData &idata, IntersectionData exclude)
-{	
-	node->intersectsWithAnyNode(ray, node, idata, exclude/*, mat4(), mat4()*/);
+RayTracer::RayTracer(	glm::mat4 trans1, glm::mat4 rot, glm::mat4 scale, 
+						glm::mat4 trans2, glm::vec3 ambient, glm::vec3 eye,
+						SceneNode* root)
+	: 	m_trans1(trans1), 
+		m_rot(rot),
+		m_scale(scale),
+		m_trans2(trans2),
+		m_ambient(ambient),
+		m_eye(eye),
+		m_root(root)
+{
 }
 
-double getLightFactorKD(const Light *light, Ray *lightray, const IntersectionData idata)
+RayTracer::~RayTracer()
+{
+}
+
+void RayTracer::intersectsWithAny(Ray *ray, IntersectionData &idata, IntersectionData exclude)
+{	
+	m_root->intersectsWithAnyNode(ray, m_root, idata, exclude);
+}
+
+double RayTracer::getLightFactorKD(const Light *light, Ray *lightray, const IntersectionData idata)
 {
 	double r = glm::length(light->position - lightray->eye);
 	double falloffSum = light->falloff[0] + light->falloff[1] * r + light->falloff[2] * r * r;
@@ -25,7 +44,7 @@ double getLightFactorKD(const Light *light, Ray *lightray, const IntersectionDat
 	return lightFactor;
 }
 
-double getLightFactorKS(const Light *light, Ray *lightray, Ray *ray, const IntersectionData idata)
+double RayTracer::getLightFactorKS(const Light *light, Ray *lightray, Ray *ray, const IntersectionData idata)
 {
 	vec3 normal = idata.prim->getNormal(lightray->eye);
 	Ray v(lightray->eye, ray->eye-lightray->eye);
@@ -41,13 +60,82 @@ double getLightFactorKS(const Light *light, Ray *lightray, Ray *ray, const Inter
 }
 
 
-bool lightObstructed(Ray *lightRay, SceneNode *node, IntersectionData idata)
+bool RayTracer::lightObstructed(Ray *lightRay, IntersectionData idata)
 {
 	IntersectionData other_idata;
-	intersectsWithAny(lightRay, node, other_idata, idata);
+	intersectsWithAny(lightRay, other_idata, idata);
 	
 	if(other_idata.pmat) return true;
 	else return false;
+}
+
+glm::vec4 RayTracer::calculateWorldCoord(glm::vec4 coord)
+{
+	return m_trans2 * m_rot * m_scale * m_trans1 * coord;
+}
+
+void RayTracer::shootRay(int x, int y, const std::list<Light *> & lights, Image & image)
+{
+	IntersectionData idata = IntersectionData();
+	vec4 coord = glm::vec4(x,y,0,1);
+
+	vec4 worldCoord = calculateWorldCoord(coord);
+
+	Ray ray = Ray(m_eye, vec3(worldCoord)-m_eye);
+	double overallShininess = 1;
+	for(int depth = 0; depth < MAX_DEPTH; depth++)
+	{
+		vec3 colour(0,0,0);
+		vec3 normal(0,0,0);
+		float epsilon = 0.01f;
+		idata = IntersectionData();
+		intersectsWithAny(&ray, idata, IntersectionData());
+		
+		glm::vec3 intersectionPt = ray.eye + (idata.t * ray.direction);
+
+		if(!idata.pmat && depth == 0)
+		{
+			float backgroundColy = 0.01 * y;
+			float backgroundColx = 0.001 * x;
+			image(x, y, 0) = 0.0f;
+			image(x, y, 1) = backgroundColx;
+			image(x, y, 2) = backgroundColy;
+			break;
+		}
+		else if(idata.pmat)
+		{
+			colour = m_ambient*idata.pmat->m_kd;
+			for(const Light * light : lights) {
+				Ray lightRay(intersectionPt+epsilon, light->position-(intersectionPt+epsilon));
+				if(!lightObstructed(&lightRay, idata))
+				{
+					double lightFactorKD = getLightFactorKD(light,&lightRay,idata);
+					double lightFactorKS = getLightFactorKS(light,&lightRay,&ray,idata);
+					if(lightFactorKD >= 0)
+					{
+						colour += light->colour*lightFactorKD*idata.pmat->m_kd;
+					}
+					if(lightFactorKS >= 0)
+					{
+						colour += light->colour*lightFactorKS*idata.pmat->m_ks;
+					}
+					normal = idata.prim->getNormal(intersectionPt);
+					if(depth != 0)
+					{
+						colour *= overallShininess;
+						double shininessPct = (double)idata.pmat->m_shininess/100;
+						overallShininess *= shininessPct;
+					}
+				}
+			}
+		}
+		if(colour == vec3(0,0,0)) break;
+		image(x, y, 0) += colour.x;
+		image(x, y, 1) += colour.y;
+		image(x, y, 2) += colour.z;
+		vec3 r = normalize(ray.direction) + 2*dot(normalize(-ray.direction),normalize(normal))*normalize(normal);
+		ray = Ray(intersectionPt+epsilon*r, r);
+	}
 }
 
 void drawLoadBar(int percentageDone)
@@ -86,16 +174,6 @@ void RayTrace(
 ) {
 
 	float d = 100.0f;
-	float z;
-	float t;
-	float light;
-	int xprime, yprime;
-
-	IntersectionData idata;
-
-	glm::vec4 coord;
-	glm::vec4 worldCoord;
-	Ray ray;
 
 	size_t nx = image.height();
 	size_t ny = image.width();
@@ -134,71 +212,24 @@ void RayTrace(
 	std::cout << "\t}" << std::endl;
 	std:: cout <<")" << std::endl;
 
+	RayTracer *myRayTracer = new RayTracer(trans1, rot, scale, trans2, ambient, eye, root);
+
+	std::vector<std::thread> myThreads;
+
 	for (uint y = 0; y < nx; ++y) {
 		drawLoadBar(100*y/nx);
 		for (uint x = 0; x < ny; ++x) {
-			idata = IntersectionData();
-			coord = glm::vec4(x,y,0,1);
-
-			worldCoord = trans2 * rot * scale * trans1 * coord;
-
-			ray = Ray(eye, vec3(worldCoord)-eye);
-			double overallShininess = 1;
-			for(int depth = 0; depth < MAX_DEPTH; depth++)
-			{
-				vec3 colour(0,0,0);
-				vec3 normal(0,0,0);
-				float epsilon = 0.01f;
-				idata = IntersectionData();
-				intersectsWithAny(&ray, root, idata, IntersectionData());
-				
-				glm::vec3 intersectionPt = ray.eye + (idata.t * ray.direction);
-
-				if(!idata.pmat && depth == 0)
-				{
-					float backgroundColy = 0.01 * y;
-					float backgroundColx = 0.001 * x;
-					image(x, y, 0) = 0.0f;
-					image(x, y, 1) = backgroundColx;
-					image(x, y, 2) = backgroundColy;
-					break;
-				}
-				else if(idata.pmat)
-				{
-					colour = ambient*idata.pmat->m_kd;
-					for(const Light * light : lights) {
-						Ray lightRay(intersectionPt+epsilon, light->position-(intersectionPt+epsilon));
-						if(!lightObstructed(&lightRay, root, idata))
-						{
-							double lightFactorKD = getLightFactorKD(light,&lightRay,idata);
-							double lightFactorKS = getLightFactorKS(light,&lightRay,&ray,idata);
-							if(lightFactorKD >= 0)
-							{
-								colour += light->colour*lightFactorKD*idata.pmat->m_kd;
-							}
-							if(lightFactorKS >= 0)
-							{
-								colour += light->colour*lightFactorKS*idata.pmat->m_ks;
-							}
-							normal = idata.prim->getNormal(intersectionPt);
-							if(depth != 0)
-							{
-								colour *= overallShininess;
-								double shininessPct = (double)idata.pmat->m_shininess/100;
-								overallShininess *= shininessPct;
-							}
-						}
-					}
-				}
-				if(colour == vec3(0,0,0)) break;
-				image(x, y, 0) += colour.x;
-				image(x, y, 1) += colour.y;
-				image(x, y, 2) += colour.z;
-				vec3 r = normalize(ray.direction) + 2*dot(normalize(-ray.direction),normalize(normal))*normalize(normal);
-				ray = Ray(intersectionPt+epsilon*r, r);
-			}
+			myRayTracer->shootRay(x,y,lights,image);
 		}
 	}
 	drawLoadBar(100);
+	
+	for(std::thread &t : myThreads)
+	{
+		t.join();
+	}
+
 	cout << endl;
+
+	delete myRayTracer;
 }
--- src/RayTracer.hpp
+++ src/RayTracer.hpp
@@ -25,3 +25,35 @@ void RayTrace(
 		const glm::vec3 & ambient,
 		const std::list<Light *> & lights
 );
+
+// todo ebonn
+class RayTracer
+{
+public:
+
+	RayTracer(	glm::mat4 trans1, glm::mat4 rot, glm::mat4 scale, 
+				glm::mat4 trans2, glm::vec3 ambient, glm::vec3 eye,
+				SceneNode *root);
+	~RayTracer();
+
+	void shootRay(int x, int y, const std::list<Light *> & lights, Image & image);
+
+	glm::vec4 calculateWorldCoord(glm::vec4 coord);
+
+private:
+
+	glm::mat4 m_trans1;
+	glm::mat4 m_rot;
+	glm::mat4 m_scale;
+	glm::mat4 m_trans2;
+	glm::vec3 m_ambient;
+	glm::vec3 m_eye;
+
+	SceneNode *m_root;
+
+	void intersectsWithAny(Ray *ray, IntersectionData &idata, IntersectionData exclude);
+	double getLightFactorKD(const Light *light, Ray *lightray, const IntersectionData idata);
+	double getLightFactorKS(const Light *light, Ray *lightray, Ray *ray, const IntersectionData idata);
+	bool lightObstructed(Ray *lightRay, IntersectionData idata);
+
+};
